973d8335acada6ba501cca006cb785543ae80941
Christina Houck <31422523+christinahouck@users.noreply.github.com>
2019-04-23 13:18:11 -0700

    Initial commit

---
+++ js/scripts.js
@@ -0,0 +1,65 @@
+// Create the X button and add it to the end of the list items
+var myNodelist = document.getElementsByTagName("LI");
+var i;
+for (i = 0; i < myNodelist.length; i++) {
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  myNodelist[i].appendChild(span);
+}
+
+// Add Display:none when the X button is clicked
+var close = document.getElementsByClassName("close");
+var i;
+for (i = 0; i < close.length; i++) {
+  close[i].onclick = function() {
+    var div = this.parentElement;
+    div.style.display = "none";
+  }
+}
+
+// Add a check when you complete a list item
+var list = document.querySelector('ul');
+list.addEventListener('click',function(ev) {
+  if (ev.target.tagName === 'LI') {
+    ev.target.classList.toggle('checked');
+  }
+}, false);
+
+// Add a new list item when text is inputted
+function newElement(){
+  var li = document.createElement("li");
+  var inputValue = document.getElementById("myInput").value;
+  var t = document.createTextNode(inputValue);
+  li.appendChild(t);
+  if (inputValue === '') {
+    alert("Add text to the box!")
+  } else {
+    document.getElementById("myUL").appendChild(li);
+  }
+  document.getElementById("myInput").value = "";
+
+
+var span = document.createElement("SPAN");
+var txt = document.createTextNode("\u00D7");
+span.className = "close";
+span.appendChild(txt);
+li.appendChild(span);
+
+for (i = 0; i < close.length; i++) {
+  close[i].onclick = function(){
+    var div = this.parentElement;
+    div.style.display = "none";
+  }
+ }
+}
+
+const dateElement = document.getElementById("date");
+const options = {
+  weekday: 'long',
+  month: 'long',
+  day: 'numeric',
+};
+const today = new Date();
+dateElement.innerHTML = today.toLocaleDateString("en-US", options);
